8c5c0c89bb8e7d3e200f76b6b12fe37f59c7b6af
steveycee <stevey.cee@gmail.com>
2024-03-03 19:56:49 +0000

    chore: tidy abit, add some npm scripts for qol

--- consumer.js
+++ consumer.js
@@ -1,7 +1,3 @@
-// fetch(url, {
-//   method: "GET", // default, so we can ignore
-// });
-
 fetch("http:/localhost:3000/nametest")
   .then((response) => response.json())
   .then((json) => console.log(json));
